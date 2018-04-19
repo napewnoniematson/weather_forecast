62bc31037afcd8174bfadc464b54b0da9e106e45
Matson <mathias94@interia.eu>
2018-04-19 11:28:02 +0200

    little func test

--- app/src/main/java/com/matsondream/weatherforecast/MainActivity.kt
+++ app/src/main/java/com/matsondream/weatherforecast/MainActivity.kt
@@ -2,11 +2,23 @@ package com.matsondream.weatherforecast
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initSearchButtonOnClick()
+    }
+
+
+    private fun initSearchButtonOnClick() {
+        searchImgBtn.setOnClickListener {
+            val city = cityET.text.toString()
+            val country = countryET.text.toString()
+
+            println("City: ${city}, country: ${country}")
+        }
     }
 }
